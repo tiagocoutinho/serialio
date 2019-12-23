737eb0c14052f8a6006db6a90b4b93614c200d72
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2019-12-23 12:24:43 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://tiagocoutinho.github.io/serialio/',
-    version='0.1.0',
+    version='0.1.1',
     python_requires='>=3.5',
     zip_safe=True
 )
