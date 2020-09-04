f66c803eb6a9cd48d0a4c83c7be6dc578ea061a1
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-09-04 16:45:02 +0200

    Force minimum sockio version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     ],
     description="Concurrency agnostic serialio API",
     license="GPLv3+",
-    install_requires=["pyserial", "sockio"],
+    install_requires=["pyserial", "sockio>=0.10"],
     long_description=description,
     long_description_content_type="text/markdown",
     keywords="serial, rs232, rcf2217, socket, tcp, ser2net",
